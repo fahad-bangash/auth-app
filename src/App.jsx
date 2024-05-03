824704a5b6b8247a4122e87fff691fd89e7a6bd5
Fahad Bangash <fahadbangash816@gmail.com>
2024-05-04 00:58:19 +0500

    add pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="bg-red-200 text-center ">
-        This is Header for Tailwind css
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
