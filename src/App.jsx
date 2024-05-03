51b99c10746d9e6c72ac863e5bb70fa5b1d67905
FAHAD <fahad.7040874@talmeez.pk>
2024-05-04 00:36:50 +0500

    First Commit Design

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="bg-red-200 text-center ">
+        This is Header for Tailwind css
+      </h1>
+    </div>
+  );
+};
+
+export default App;
